b1acddc8765aa551541a0c95cedb3b64676e93c7
Ben Balter <ben.balter@github.com>
2015-01-28 15:06:28 -0800

    better tests

--- test/so_far_so_good_bin_test.rb
+++ test/so_far_so_good_bin_test.rb
@@ -1,8 +1,10 @@
 require 'helper'
+require 'open3'
 
 class TestSoFarSoGoodBin < Minitest::Test
   should "work via CLI" do
-    output = `bundle exec far 52.222-3`
+    output, status = Open3.capture2e("bundle", "exec", "bin/far", "52.222-3")
+    assert_equal 0, status.exitstatus
     assert output.include? "Convict Labor"
   end
 end
